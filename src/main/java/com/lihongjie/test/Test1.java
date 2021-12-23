03da818dce91651b436df96dc12043c2167f8902
lihongjie <2242786017@qq.com>
2021-12-23 21:06:34 +0800

    git实战训练营_回退指定文件到某个版本第一次提交;

---
+++ src/main/java/com/lihongjie/test/Test1.java
@@ -0,0 +1,8 @@
+package com.lihongjie.test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello i am git practice demo!");
+        System.out.println("第一次提交");
+    }
+}
